9c1a9cc5865cca974d50929086a826a5f27f6cdb
Angela Anjorin <angela.anjorin@gmail.com>
2024-05-22 20:50:29 +0000

    Create an empty list called display. For each letter inthe randomly chosen word, insert a_ to display

--- run.py
+++ run.py
@@ -6,6 +6,11 @@ chosen_word = random.choice(word_list)
 #Testing code
 print(f'The chosen word is {chosen_word}.')
 
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+
 guess = input("Guess a letter: ").lower()
 for letter in chosen_word:
     if letter == guess:
