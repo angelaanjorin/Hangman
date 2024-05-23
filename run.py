d94d7e7ba38d971e3ec77948e08d43dbd6520095
Angela Anjorin <angela.anjorin@gmail.com>
2024-05-23 10:04:28 +0000

    Install pip awoc and import awoc package. Initialize awoc and get the nations of europe and assign them to a variable called nations of europe.

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 
+import awoc
 import random
-word_list = ["amsterdam", "barcelona", "helsinki"]
-chosen_word = random.choice(word_list)
+
+my_world = awoc.AWOC()
+nations_of_europe = my_world.get_countries_list_of ('Europe')
+chosen_nation = random.choice(nations_of_europe).lower()
 
 #Testing code
-print(f'The chosen word is {chosen_word}.')
+print(f'The chosen nation is {chosen_nation}.')
 
 display = []
-word_length = len(chosen_word)
+word_length = len(chosen_nation)
 for _ in range(word_length):
     display += "_"
 
@@ -18,7 +21,7 @@ while not end_of_game:
 
     #check guessed letter
     for position in range(word_length):
-        letter = chosen_word[position]
+        letter = chosen_nation[position]
         #print(f"current position: {position}\n current letter: {letter}\n guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
