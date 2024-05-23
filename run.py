3c463b6528f877c1b11262f1793525b5adb3e0f3
Angela Anjorin <angela.anjorin@gmail.com>
2024-05-23 11:44:05 +0000

    Create an empty list for wronged guessed letters. If the user guesses a letter wrong it gets added to this list and displayed to the user. Display the lives countdown. If the user loses, display the chosen nation they were trying to guess. If the guessed letter is in the display or wrong letter list, notify the user of this. Lives should not be reduced.

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 
 import awoc
 import random
+import os
+#clear = lambda: os.system('cls') #on Windows System
+#j
+#os.system('clear') #on Linux System
 
 my_world = awoc.AWOC()
 nations_of_europe = my_world.get_countries_list_of ('Europe')
@@ -9,6 +13,8 @@ word_length = len(chosen_nation)
 
 end_of_game = False
 lives = 6
+
+wrong_letter_list = []
 
 #import logo
 from hangman_art import logo
@@ -23,9 +29,21 @@ display = []
 for _ in range(word_length):
     display += "_"
 
-while not end_of_game:
-    guess = input("Guess a letter: ").lower()
 
+while not end_of_game:
+    if wrong_letter_list != []:
+        print(f'Wrong letters: {wrong_letter_list}')
+    guess = input("Guess a letter: ").lower()
+    #clear()
+
+    #prompts for already guessed letter
+    if guess in display:
+        print(f"You´ve alread guessed {guess}")
+
+    elif guess in chosen_nation:
+        print(f"Great, {guess} is in the word! Keep going!")
+    
+        
     #check guessed letter
     for position in range(word_length):
         letter = chosen_nation[position]
@@ -34,17 +52,25 @@ while not end_of_game:
             display[position] = letter
 
     #check if letter is wrong.
-    if guess not in chosen_nation:
+    if guess in wrong_letter_list:
+        print(f"You´ve already guessed {guess}")
+    elif guess not in chosen_nation:
+        wrong_letter_list.append(guess)
         print(f"You guessed {guess}, that´s not in the word. You lose a life.")
 
         lives -= 1
+    
+
         if lives == 0:
             end_of_game = True
             print("You lose.")
+            print(f"The word was {chosen_nation}")
 
     print(f"{' '.join(display)}")
+
+    print(f'Lives left: {lives}')
     
-    #check if use has got all letters.End of game.
+    #check if user has got all letters.End of game.
     if "_" not in display:
         end_of_game = True
         print("You win!")
