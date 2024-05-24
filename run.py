5b564fb1cf5a22afc7b31b6d6cefb671ec58640c
Angela Anjorin <angela.anjorin@gmail.com>
2024-05-24 10:52:49 +0000

    Prevent the user from inputting invalid inputs like numbers by checking of the input is a letter. Add a section of the code to capture if the user inputs the whole chosen word correctly and end game. If not notify them that the word inputed is wrong and they loose an attempt.By the same repeated wrong word they don´t loose an attempt.

--- run.py
+++ run.py
@@ -35,15 +35,6 @@ chosen_word = random.choice(word_list).lower()
 print(f'Pssst...The chosen nation is {chosen_word}.')
 
 word_length = "_"  * len(chosen_word)
-#print(nations_of_europe)
-display = []
-guessed_word = []
-wrong_letter_list = []
-end_of_game = False
-attempts = 6
-
-
-
 #create blanks
 def word_dash(word_length):
     for i in word_length:
@@ -51,58 +42,88 @@ def word_dash(word_length):
 #for _ in range(word_length):
    # display += "_"
 
+#print(nations_of_europe)
+correct_letters = []
+guessed_word = []
+wrong_letter_list = []
+end_of_game = False
+attempts = 6
+
+print(f"""{Fore.YELLOW}YOU HAVE TO GUESS A WORD WITH {len(chosen_word)} LETTERS""")
+word_dash(word_length)
+print("\n")
 
 while not end_of_game:
     if wrong_letter_list != []:
         print(f'Wrong letters: {wrong_letter_list}')
     guess = input("Guess a letter: ").lower()
     clean()
-
+    if len(guess) == 1 and guess.isalpha():
     #prompts for already guessed letter
-    if guess in display:
-        print(f"You´ve already guessed {guess}")
+        if guess in word_length:
+            print(f"You´ve already guessed {guess} correctly")
 
-    elif guess in chosen_word:
-        print(f"Great, {guess} is in the word!")
-    
-        
-    #check guessed letter
-    word_as_list = list(word_length)
-    indices = [i for i, letter in enumerate(chosen_word) if letter == guess]
-    for index in indices:
-        word_as_list[index] = guess
-    word_length = "".join(word_as_list)
+        elif guess in chosen_word:
+            print(f"Great, {guess} is in the word!")
 
-    #for position in range(word_length):
-        #letter = chosen_word[position]
-        #print(f"current position: {position}\n current letter: {letter}\n guessed letter: {guess}")
-        #if letter == guess:
-            #display[position] = letter
+            
+        #check guessed letter
+        word_as_list = list(word_length)
+        indices = [i for i, letter in enumerate(chosen_word) if letter == guess]
+        for index in indices:
+            word_as_list[index] = guess
+        word_length = "".join(word_as_list)
 
-    #check if letter is wrong.
-    if guess in wrong_letter_list:
-        print(f"You´ve already guessed {guess}")
-    elif guess not in chosen_word:
-        wrong_letter_list.append(guess)
-        print(f"You guessed {guess}, that´s not in the word.")
-
-        attempts -= 1
-    
-
-        if attempts == 0:
+        #check if user has got all letters.End of game.
+        if "_" not in word_as_list:
             end_of_game = True
-            print("You lose.")
-            print(f"The word was {chosen_word}")
+            print("You win!")
+        #for position in range(word_length):
+            #letter = chosen_word[position]
+            #print(f"current position: {position}\n current letter: {letter}\n guessed letter: {guess}")
+            #if letter == guess:
+                #display[position] = letter
+
+        #check if letter is wrong.
+        if guess in wrong_letter_list:
+            print(f"You´ve already guessed {guess} wrongly")
+        elif guess not in chosen_word:
+            wrong_letter_list.append(guess)
+            print(f"You guessed {guess}, that´s not in the word.")
+
+            attempts -= 1
+
+            if attempts == 0:
+                end_of_game = True
+                print("You lose.")
+                print(f"The word was {chosen_word}")
+
+    elif len(guess) >= 2  and guess.isalpha():
+        if guess == chosen_word:
+            end_of_game = True
+            print(f"""{Fore.YELLOW}\n Whoohh,You have guessed the word {guess} already!!!\n You Win!!\n""")
+
+        elif guess in guessed_word:
+            print(f"{Fore.RED}\n\t You´ve already guessed {guess} wrongly")
+        
+        elif guess != chosen_word:
+            print(f"{Fore.RED}\n\t{guess}, is not the Word, try again!")
+            attempts -= 1
+            guessed_word.append(guess)
+        
+        else:
+            end_of_game = True
+            word_length = chosen_word
+    else:
+        print(f"{Fore.RED}\n\t INVALID INPUT!\n")
 
     #print(f"{' '.join(word_as_list)}")
     word_dash(word_length)
+    print("\n")
 
     print(f'Attempts left: {attempts}')
     
-    #check if user has got all letters.End of game.
-    if "_" not in word_as_list:
-        end_of_game = True
-        print("You win!")
+
 
     #import stages of hangman
     from hangman_art import stages
