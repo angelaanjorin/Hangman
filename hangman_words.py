c3f1fbc17054dbe0683be52d1898d14bc206c949
Angela Anjorin <angela.anjorin@gmail.com>
2024-05-24 07:11:59 +0000

    Add cities of Europe to the word list in the file hangman_words and write a function to randomly choose a word from that list and define the chosen word as a variable.

---
+++ hangman_words.py
@@ -0,0 +1,59 @@
+import random
+
+word_list[
+    "Lisbon",
+    "Barcelona",
+    "Prague",
+    "Berlin",
+    "Stockholm",
+    "Helsinki",
+    "Istanbul",
+    "Moscow",
+    "London",
+    "Madrid",
+    "Rom",
+    "Kiev",
+    "Paris",
+    "Vienna",
+    "Minsk",
+    "Hamburg",
+    "Warsaw",
+    "Bucharest",
+    "Belgrade",
+    "Copenhagen",
+    "Brussels",
+    "Athens",
+    "Budapest",
+    "Munich",
+    "Frankfurt",
+    "Milan",
+    "Birmingham",
+    "Manchester",
+    "Sofia",
+    "Dublin",
+    "Bern",
+    "Chisinau",
+    "Bratislava",
+    "Ljubljana",
+    "Luxembourg",
+    "Monaca",
+    "Nicosia",
+    "Nuuk",
+    "Oslo",
+    "Podgorica",
+    "Reykavik",
+    "Serajevo",
+    "Tallinn",
+    "Tirana",
+    "Vaduz",
+    "Valletta",
+    "Vatican",
+    "Vilnius",
+    "Zagreb"
+]
+
+"""Choose a word randomly from the word list
+"""
+def get_word ():
+    chosen_word = random.choice(word_list)
+    return chosen.word.lower()
