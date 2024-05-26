705985630713f02c6bebe4f7ae41df721db4a63b
Angela Anjorin <angela.anjorin@gmail.com>
2024-05-26 11:55:32 +0000

    Define user´s inputed name and city to a global variable so that these can be called in other functions. In the update_worksheet function parse the variables into the function, so that they can be appended as a row to the worksheet(score). Delete commented out code that is no more needed.

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-
-#import awoc
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -12,11 +10,12 @@ SCOPE = [
 import random
 import os
 
+#Add date to google worksheet
 import datetime
 date = datetime.datetime.today()
 today_date = date.strftime("%d/%m/%Y")
 
-
+#Add color to text 
 import colorama
 from colorama import Fore
 colorama.init(autoreset = True)
@@ -32,24 +31,20 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hangman_leaderboard')
 
 scores = SHEET.worksheet('scores')
-
 data = scores.get_all_values()
 
-#print(data)
 CORRECT_LETTER_SCORE = 5
 EXTRA_SCORE = 50
 FULL_WORD_SCORE = 500
+score = 0
 
-
-
-#Add date and time to scores
-#Add the calculated scores from end of game to scores
 #Retrieve and show user thier scores
 # Ask user if they want to see the top five scored users
 
-#import logo
-#from hangman_art import logo
-#def welcome_message():
+def welcome_message():
+    """Collect User´s name and city and set them as global variables so that they can be called in another funtion.
+    """
+    global player_name, player_city
     #print(f'{Fore.GREEN} {logo}')
     #typewriter (""" W E L C O M E   T O  T H E  H A N G M A N  G A M E ! !\n """)
     #print(f"{Fore.CYAN} HERE ARE THE RULES: {game_info[0]}")
@@ -68,154 +63,146 @@ FULL_WORD_SCORE = 500
         #else:
             #print(f"{Fore.RED}Invalid input. Please try again")
             #play_game = False
-    #Collect name and city from the user        
-if __name__ == '__main__':
-    while True:
-        player_name = input(f"{Fore.CYAN}Please enter your name:\n").strip().lower()
-        if len(player_name) == 0:
-            print(f"{Fore.RED}Invalid input!")
-            continue
-        else:
-            break
-    while True:
-        player_city = input(f"{Fore.CYAN}Please enter your city:\n").strip().lower()
-        if len(player_name) == 0:
-            print(f"{Fore.RED}Invalid input!")
-            continue
-        else:
-            break
+
+    #Collect user´s name and city       
+    if __name__ == '__main__':
+        while True:
+            player_name = input(f"{Fore.CYAN}Please enter your name:\n").strip().lower()
+            
+            if len(player_name) == 0:
+                print(f"{Fore.RED}Invalid input!")
+                continue
+            else:
+                break
+        while True:
+            player_city = input(f"{Fore.CYAN}Please enter your city:\n").strip().lower()
+            if len(player_name) == 0:
+                print(f"{Fore.RED}Invalid input!")
+                continue
+            else:
+                break
     #typewriter (""" Y O U  A R E  B R A V E   T O  P L A Y\t\nT H I S   G A M E   B Y   T H E   W A Y ! !
     #\t\n\nG O O D   L U C K ! !\n
    # """)
 #     clean()
-# # To clear the screen after every iteration
-# def clean():
-# #on Windows System
-#     if os.name == 'nt':
-#         os.system('cls')
-# # on macOS and Linux System
-#     else:
-#         os.system('clear') 
 
-# #my_world = awoc.AWOC()
-# #nations_of_europe = my_world.get_countries_list_of ('Europe')
+# To clear the screen after every iteration
+def clean():
+#on Windows System
+    if os.name == 'nt':
+        os.system('cls')
+# on macOS and Linux System
+    else:
+        os.system('clear') 
 
-# chosen_word = random.choice(word_list).lower()
+chosen_word = random.choice(word_list).lower()
 # #get_word ()
-# def play_game(chosen_word):
-#     #Testing code
-#     print(f'Pssst...The chosen nation is {chosen_word}.')
+def play_game(chosen_word):
+    #Testing code
+    print(f'{Fore.LIGHTMAGENTA_EX}Pssst...The chosen nation is {chosen_word}.')
 
-#     word_length = "_"  * len(chosen_word)
-#     #create blanks
-#     def word_dash(word_length):
-#         for i in word_length:
-#             print(i, end=" ")
-#     #for _ in range(word_length):
-#     # display += "_"
+    word_length = "_"  * len(chosen_word)
+    #create blanks
+    def word_dash(word_length):
+        for i in word_length:
+            print(i, end=" ")
+    
+    correct_letters = []
+    guessed_word = []
+    wrong_letter_list = []
+    guessed_right = 0
+    score = 0
+    end_of_game = False
+    attempts = 6
 
-#     #print(nations_of_europe)
-#     correct_letters = []
-#     guessed_word = []
-#     wrong_letter_list = []
-#     guessed_right = 0
-#     score = 0
-#     end_of_game = False
-#     attempts = 6
+    print(f"""{Fore.YELLOW}YOU HAVE TO GUESS A WORD WITH {len(chosen_word)} LETTERS""")
+    print('\n')
+    word_dash(word_length)
+    print("\n")
 
-#     print(f"""{Fore.YELLOW}YOU HAVE TO GUESS A WORD WITH {len(chosen_word)} LETTERS""")
-#     print('\n')
-#     word_dash(word_length)
-#     print("\n")
+    while not end_of_game:
+        if wrong_letter_list != []:
+            print(f'{Fore.RED}Wrong letters: {wrong_letter_list}')
+            print('\n')
+        guess = input("Guess a letter: ").lower()
+        clean()
 
-#     while not end_of_game:
-#         if wrong_letter_list != []:
-#             print(f'{Fore.RED}Wrong letters: {wrong_letter_list}')
-#             print('\n')
-#         guess = input("Guess a letter: ").lower()
-#         clean()
+        if len(guess) == 1 and guess.isalpha():
+        #prompts for already guessed letter
+            if guess in word_length:
+                print(f"You´ve already guessed {guess} correctly")
 
-#         if len(guess) == 1 and guess.isalpha():
-#         #prompts for already guessed letter
-#             if guess in word_length:
-#                 print(f"You´ve already guessed {guess} correctly")
-
-#             elif guess in chosen_word:
-#                 print(f"Great, {guess} is in the word!")
-#                 correct_letters.append(guess)
-#                 guessed_right += 1
-#                 score += CORRECT_LETTER_SCORE
+            elif guess in chosen_word:
+                print(f"Great, {guess} is in the word!")
+                correct_letters.append(guess)
+                guessed_right += 1
+                score += CORRECT_LETTER_SCORE
    
-#             #check guessed letter
-#             word_as_list = list(word_length)
-#             indices = [i for i, letter in enumerate(chosen_word) if letter == guess]
-#             for index in indices:
-#                 word_as_list[index] = guess
-#             word_length = "".join(word_as_list)
+            #check guessed letter
+            word_as_list = list(word_length)
+            indices = [i for i, letter in enumerate(chosen_word) if letter == guess]
+            for index in indices:
+                word_as_list[index] = guess
+            word_length = "".join(word_as_list)
 
-#             #check if user has got all letters.End of game.
-#             if "_" not in word_as_list:
-#                 end_of_game = True
-#                 print("You win!")
-#                 print(f"The word was {chosen_word}")
-#                 print(f"Score:{score}")
-#                 repeat_game()
+            #check if user has got all letters.End of game.
+            if "_" not in word_as_list:
+                end_of_game = True
+                print("You win!")
+                print(f"The word was {chosen_word}")
+                #print(f"Score:{score}")
+                #repeat_game()
 
-#             #for position in range(word_length):
-#                 #letter = chosen_word[position]
-#                 #print(f"current position: {position}\n current letter: {letter}\n guessed letter: {guess}")
-#                 #if letter == guess:
-#                     #display[position] = letter
+            #check if letter is wrong.
+            if guess in wrong_letter_list:
+                print(f"You´ve already guessed {guess} wrongly")
+            elif guess not in chosen_word:
+                wrong_letter_list.append(guess)
+                print(f"You guessed {guess}, that´s not in the word.")
 
-#             #check if letter is wrong.
-#             if guess in wrong_letter_list:
-#                 print(f"You´ve already guessed {guess} wrongly")
-#             elif guess not in chosen_word:
-#                 wrong_letter_list.append(guess)
-#                 print(f"You guessed {guess}, that´s not in the word.")
+                attempts -= 1
 
-#                 attempts -= 1
+                if attempts == 0:
+                    end_of_game = True
+                    print("You lose.")
+                    #print(f"The word was {chosen_word}")
+                    #repeat_game()
 
-#                 if attempts == 0:
-#                     end_of_game = True
-#                     print("You lose.")
-#                     print(f"The word was {chosen_word}")
-#                     repeat_game()
-
-#         #check for word inputs
-#         elif len(guess) >= 2  and guess.isalpha():
-#             if guess == chosen_word:
-#                 end_of_game = True
-#                 print(f"""{Fore.YELLOW}\n Whoohh,You have guessed the word {guess} already!!!\n You Win!!\n""")
-#                 score += FULL_WORD_SCORE - score
-#                 print(f"Score:{score}")
-#                 repeat_game()
-#             elif guess in guessed_word:
-#                 print(f"{Fore.RED}\n\t You´ve already guessed {guess} wrongly")
+        #check for word inputs
+        elif len(guess) >= 2  and guess.isalpha():
+            if guess == chosen_word:
+                end_of_game = True
+                print(f"""{Fore.YELLOW}\n Whoohh,You have guessed the word {guess} already!!!\n You Win!!\n""")
+                score += FULL_WORD_SCORE - score
+                #print(f"Score:{score}")
+                #repeat_game()
+            elif guess in guessed_word:
+                print(f"{Fore.RED}\n\t You´ve already guessed {guess} wrongly")
             
-#             elif guess != chosen_word:
-#                 print(f"{Fore.RED}\n\t{guess}, is not the Word, try again!")
-#                 attempts -= 1
-#                 guessed_word.append(guess)
+            elif guess != chosen_word:
+                print(f"{Fore.RED}\n\t{guess}, is not the Word, try again!")
+                attempts -= 1
+                guessed_word.append(guess)
             
-#             else:
-#                 end_of_game = True
-#                 word_length = chosen_word
-#                 score += EXTRA_SCORE
-#         else:
-#             print(f"{Fore.RED}\n\t INVALID INPUT!\n")
+            else:
+                end_of_game = True
+                word_length = chosen_word
+                score += EXTRA_SCORE
+        else:
+            print(f"{Fore.RED}\n\t INVALID INPUT!\n")
 
-#         #print(f"{' '.join(word_as_list)}")
-#         word_dash(word_length)
-#         print("\n")
-#         print(f'Attempts left: {attempts}')
-#         #print("\n")
-#         print(f"Score: {score}")
-        
+        #print(f"{' '.join(word_as_list)}")
+        word_dash(word_length)
+        print("\n")
+        print(f'Attempts left: {attempts}')
+        #print("\n")
+        print(f"Score: {score}")
 
-#         #import stages of hangman
-#         from hangman_art import stages
-#         print(stages[attempts])
+        #import stages of hangman
+        from hangman_art import stages
+        print(stages[attempts])
+    update_worksheet(data, player_name, player_city, today_date, score)
+#play_game(chosen_word)
 #         #final_score(end_of_game, chosen_word, guessed_right, score)
 
 # # def final_score(end_of_game, chosen_word, guessed_right, score):
@@ -232,21 +219,21 @@ if __name__ == '__main__':
 
 
 
-# #Repeat game      
-# def repeat_game():
-#     """Asks the user if they want to play again or not.
-#     """
-#     game = input('Are you ready to play again? Y(es) or N(o)\n').upper()
-#     if game == 'Y':
-#         clean()
-#         #print(f"Score:{score}")
-#         play_game(chosen_word)
-#     elif game == 'N':
-#         print('Goodbye!')
-#         sys.exit()
-#     else:
-#         print('Please enter a valid answer')
-#         repeat_game()
+#Repeat game      
+def repeat_game():
+    """Asks the user if they want to play again or not.
+    """
+    game = input('Are you ready to play again? Y(es) or N(o)\n').upper()
+    if game == 'Y':
+        clean()
+        #print(f"Score:{score}")
+        play_game(chosen_word)
+    elif game == 'N':
+        print('Goodbye!')
+        sys.exit()
+    else:
+        print('Please enter a valid answer')
+        repeat_game()
 
 
 # def display_score(score):
@@ -255,23 +242,22 @@ if __name__ == '__main__':
 #     print(f"\tSCORE: {score}")
 
 
-#funcitons for google worksheet
-def update_worksheet(data):
+#Add the endscores from end of game and other data from user to worksheet(scores)
+def update_worksheet(data, player_name, player_city, today_date, score):
     
     print("Updating Leaderboard...\n")
     worksheet_to_update = SHEET.worksheet('scores')
-    worksheet_to_update.append_row([
-         str(player_name[0:10]), player_city, today_date])
+    worksheet_to_update.append_row([str(player_name[0:10]), player_city, today_date, score])
     
     print('Leaderboard Updated.\n')
-update_worksheet(data)
-data = scores.get_all_values()
-print(data)
 
-# def main():
-#     welcome_message()
-#     #play_game(chosen_word)
-#     #final_score(end_of_game, chosen_word, guessed_right, score)
-#     update_worksheet(data)
-#     #repeat_game()
-# main()
+    #print(data)
+
+#Main function
+def main():
+    welcome_message()
+    play_game(chosen_word)
+    #final_score(end_of_game, chosen_word, guessed_right, score)
+    #update_worksheet(data, player_name, player_city, today_date, score)
+    repeat_game()
+main()
